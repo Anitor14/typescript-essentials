271d7a3170d9b8ff67c4f74849e264ad0991c4fa
Anitor <anitorabraham@gmail.com>
2022-10-25 16:17:55 +0100

    learnt interface

--- dist/index.js
+++ dist/index.js
@@ -17,4 +17,43 @@ employee = [
     [2, "John"],
     [3, "Abraham"],
 ];
-// Union
+//Union , You can set a variable more than one type.
+let pid;
+// Enum
+var Direction1;
+(function (Direction1) {
+    Direction1[Direction1["Up"] = 1] = "Up";
+    Direction1[Direction1["Down"] = 2] = "Down";
+    Direction1[Direction1["Left"] = 3] = "Left";
+    Direction1[Direction1["Right"] = 4] = "Right";
+})(Direction1 || (Direction1 = {}));
+var Direction2;
+(function (Direction2) {
+    Direction2["Up"] = "Up";
+    Direction2["Down"] = "Down";
+    Direction2["Left"] = "Left";
+    Direction2["Right"] = "Right";
+})(Direction2 || (Direction2 = {}));
+console.log(Direction2.Left);
+const user = {
+    id: 1,
+    name: "John",
+};
+// Type Assertion
+let cid = 1;
+// let customerId = <number>cid;
+let customerId = cid;
+// Functions
+function addNum(x, y) {
+    return x + y;
+}
+console.log(addNum(5, 3));
+// void
+function log(message) {
+    console.log(message);
+}
+const user1 = {
+    id: 1,
+    name: "John",
+};
+const p1 = 5;
--- src/index.ts
+++ src/index.ts
@@ -18,5 +18,60 @@ employee = [
   [2, "John"],
   [3, "Abraham"],
 ];
+//Union , You can set a variable more than one type.
+let pid: string | number;
+// Enum
+enum Direction1 {
+  Up = 1,
+  Down,
+  Left,
+  Right,
+}
+enum Direction2 {
+  Up = "Up",
+  Down = "Down",
+  Left = "Left",
+  Right = "Right",
+}
+console.log(Direction2.Left);
 
-// Union
+// Objects
+type User = {
+  id: number;
+  name: string;
+};
+const user: User = {
+  id: 1,
+  name: "John",
+};
+
+// Type Assertion
+let cid: any = 1;
+// let customerId = <number>cid;
+let customerId = cid as number;
+
+// Functions
+function addNum(x: number, y: number): number {
+  return x + y;
+}
+
+console.log(addNum(5, 3));
+
+// void
+function log(message: string | number): void {
+  console.log(message);
+}
+
+//Interfaces
+interface UserInterface {
+  id: number;
+  name: string;
+  age?: number;
+}
+const user1: UserInterface = {
+  id: 1,
+  name: "John",
+};
+
+type Point = number | string;
+const p1: Point = 5;
